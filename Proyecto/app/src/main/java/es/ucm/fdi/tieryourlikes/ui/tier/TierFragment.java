b1e4a7ffc34effa284455e3f23d2d02d2e5a3f65
Dani <53879220+GeoDNSO@users.noreply.github.com>
2021-04-26 20:41:16 +0200

    TierFragment añadido

---
+++ Proyecto/app/src/main/java/es/ucm/fdi/tieryourlikes/ui/tier/TierFragment.java
@@ -0,0 +1,38 @@
+package es.ucm.fdi.tieryourlikes.ui.tier;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import es.ucm.fdi.tieryourlikes.R;
+
+public class TierFragment extends Fragment {
+
+    private TierViewModel mViewModel;
+
+    public static TierFragment newInstance() {
+        return new TierFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tier_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(TierViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ Proyecto/app/src/main/java/es/ucm/fdi/tieryourlikes/ui/tier/TierViewModel.java
@@ -0,0 +1,7 @@
+package es.ucm.fdi.tieryourlikes.ui.tier;
+
+import androidx.lifecycle.ViewModel;
+
+public class TierViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
