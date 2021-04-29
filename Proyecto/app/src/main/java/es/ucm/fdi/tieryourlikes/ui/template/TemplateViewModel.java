ef8791b1d6a0f7abcfc85569206c08cde050c194
Jpz777 <49988587+Jpz777@users.noreply.github.com>
2021-04-29 14:47:18 +0200

    un poco de css

--- Proyecto/app/src/main/java/es/ucm/fdi/tieryourlikes/ui/template/TemplateViewModel.java
+++ Proyecto/app/src/main/java/es/ucm/fdi/tieryourlikes/ui/template/TemplateViewModel.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class TemplateViewModel extends ViewModel {
 
     public void createTemplate(String template_name, String template_category, String image, List<String> imageString, List<String> rowString) {
+
     }
 
     // TODO: Implement the ViewModel
