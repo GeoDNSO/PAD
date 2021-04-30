2c5470e06e913fce2df2195293e691cb9ae9f859
Daniel Sanz Mayo <dasanz04@ucm.es>
2021-04-30 14:27:23 +0200

    Vista de Perfil y funcionalidad de login (por arreglar)

--- Proyecto/app/src/main/java/es/ucm/fdi/tieryourlikes/AppConstants.java
+++ Proyecto/app/src/main/java/es/ucm/fdi/tieryourlikes/AppConstants.java
@@ -35,4 +35,7 @@ public class AppConstants {
     public static final String DB_PAGINATION_LIMIT = "limit";
     public static final int DEAFULT_LIMIT = 1;
 
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+    public static final String EMAIL = "email";
 }
