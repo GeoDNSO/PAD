5db1280e8fed974b41c92be2d178139e6f5d8f53
Dani <53879220+GeoDNSO@users.noreply.github.com>
2021-05-15 20:16:35 +0200

    Limpieza de SimpleHttp

--- Proyecto/app/src/main/java/es/ucm/fdi/tieryourlikes/networking/SimpleRequest.java
+++ Proyecto/app/src/main/java/es/ucm/fdi/tieryourlikes/networking/SimpleRequest.java
@@ -41,24 +41,10 @@ public class SimpleRequest {
     private static final String DIRECTORY = "_templateImages";
     private static final String IMAGE_DIRECTORY = SERVER_URL + "/" + DIRECTORY;
 
-    private volatile boolean finished;
     private volatile String response;
 
     public SimpleRequest(){
-        finished = false;
         response = null;
-    }
-
-    public static String getServerUrl() {
-        return SERVER_URL;
-    }
-
-    public static String getImageDirectory() {
-        return IMAGE_DIRECTORY;
-    }
-
-    public String getResponse(){
-        return this.response;
     }
 
     public Call createCall(Request request){
@@ -72,6 +58,7 @@ public class SimpleRequest {
 
         return  call;
     }
+
 
     public Request buildRequest(String postBodyString, String method, String route) {
 
@@ -101,58 +88,16 @@ public class SimpleRequest {
         return request;
     }
 
-    public static byte[] imageToBytes(String imagePath){
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inPreferredConfig = Bitmap.Config.RGB_565;
-        // Read BitMap by file path
-        Bitmap bitmap = BitmapFactory.decodeFile(imagePath, options);
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-
-        return stream.toByteArray();
+    public static String getServerUrl() {
+        return SERVER_URL;
     }
 
-    public Request buildRequest(String postBodyString, String[] filesPath, String method, String route) {
+    public static String getImageDirectory() {
+        return IMAGE_DIRECTORY;
+    }
 
-        MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
-        RequestBody body = RequestBody.create(postBodyString, mediaType);
-
-        Request request = null;
-
-        //TODO MIRAR SI HACE FALTA ELPOST BODY STRING O SI PIDER TODOS LOS DATOS EN EL REQUEST
-        //RequestBody
-        MultipartBody.Builder requestBodyBuilder = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
-                .addFormDataPart("n_images", ""+filesPath.length)
-                .addFormDataPart("member_id", postBodyString);
-
-        for(int i = 0; i < filesPath.length; ++i){
-            byte[] byteArray = imageToBytes(filesPath[i]);
-            requestBodyBuilder.addFormDataPart("image_"+i, "img_" + i + ".jpg", RequestBody.create(MediaType.parse("image/*jpg"), byteArray));
-        }
-
-
-
-
-        if(method.equals(AppConstants.METHOD_GET)){
-            request = new Request.Builder()
-                    .get()
-                    .url(SERVER_URL + route)//Ej http://10.0.0.2:5000/login
-                    .header("Accept", "application/json")
-                    .header("Content-Type", "application/json")
-                    .header("Connection", "close")
-                    .build();
-        }else{
-            request = new Request.Builder()
-                    .method(method, body)
-                    .url(SERVER_URL + route)//Ej http://10.0.0.2:5000/login
-                    .header("Accept", "application/json")
-                    .header("Content-Type", "application/json")
-                    .header("Connection", "close")
-                    .build();
-        }
-
-        return request;
+    public String getResponse(){
+        return this.response;
     }
 
 
@@ -200,44 +145,4 @@ public class SimpleRequest {
         return exists;
     }
 
-    //isSuccessful esta sobrecargado...
-    public boolean isSuccessful(Response response) {
-        if (response == null) {
-            return false;
-        }
-        try {
-            this.response = response.body().string();
-            JSONObject jsonResponse = new JSONObject(this.response);
-            return jsonResponse.get("exito").equals("true");
-        } catch (JSONException | IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    public boolean isSuccessful(String response) {
-        if (response == null) {
-            return false;
-        }
-        try {
-            JSONObject jsonResponse = new JSONObject(response);
-            return jsonResponse.get("exito").equals("true");
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    public boolean isSuccessful() {
-        if (response == null) {
-            return false;
-        }
-        try {
-            JSONObject jsonResponse = new JSONObject(response);
-            return jsonResponse.get("exito").equals("true");
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
 }
